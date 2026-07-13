5ec8a64547591434b7dcaf24661420df9e7f51b7
Ali Mansour <dev.ali.mansour@gmail.com>
2026-07-13 13:04:17 +0300

    feat: Add /teaminfo endpoint using custom properties
    - Add custom properties for `coach.name` and `team.name` in `application.properties`.
    - Inject the properties into `FunRestController` using the `@Value` annotation.
    - Expose a new `@GetMapping("/teaminfo")` endpoint to display the coach and team name.

--- src/main/kotlin/dev/alimansour/springboot/demo/mycoolapp/rest/FunRestController.kt
+++ src/main/kotlin/dev/alimansour/springboot/demo/mycoolapp/rest/FunRestController.kt
@@ -1,10 +1,24 @@
 package dev.alimansour.springboot.demo.mycoolapp.rest
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class FunRestController {
+    // inject properties for: coach.name and team.name
+    @Value("\${coach.name}")
+    private lateinit var coachName: String
+
+    @Value("\${team.name}")
+    private lateinit var teamName: String
+
+    //expose new endpoints for "teaminfo"
+    @GetMapping("/teaminfo")
+    fun getTeamInfo(): String {
+        return "Coach: $coachName, Team name: $teamName"
+    }
+
     // expose "/" that return "Hello World
 
     @GetMapping("/")
