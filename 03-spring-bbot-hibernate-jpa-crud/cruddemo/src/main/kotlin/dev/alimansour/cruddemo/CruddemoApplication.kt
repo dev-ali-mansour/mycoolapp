1a3017d8c46f84018c6e2faf0ac8592ec9ef503a
Ali Mansour <dev.ali.mansour@gmail.com>
2026-07-15 16:49:17 +0300

    feat: Add methods to create and save Student objects
    - Inject `StudentDao` into the `commandLineRunner` bean.
    - Implement `createStudent()` to instantiate and save a single `Student` object, and log its generated ID.
    - Implement `createMultipleStudents()` to instantiate and save three different `Student` objects.
    - Update the `CommandLineRunner` to execute `createMultipleStudents()` upon application startup.

--- 03-spring-bbot-hibernate-jpa-crud/cruddemo/src/main/kotlin/dev/alimansour/cruddemo/CruddemoApplication.kt
+++ 03-spring-bbot-hibernate-jpa-crud/cruddemo/src/main/kotlin/dev/alimansour/cruddemo/CruddemoApplication.kt
@@ -1,18 +1,48 @@
 package dev.alimansour.cruddemo
 
+import dev.alimansour.cruddemo.dao.StudentDao
+import dev.alimansour.cruddemo.entity.Student
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class CruddemoApplication{
+class CruddemoApplication {
 
     @Bean
-    fun commandLineRunner(): CommandLineRunner {
+    fun commandLineRunner(studentDao: StudentDao): CommandLineRunner {
         return CommandLineRunner { runner ->
-            println("Hello World")
+//            createStudent(studentDao)
+            createMultipleStudents(studentDao)
         }
+    }
+
+    private fun createMultipleStudents(studentDao: StudentDao) {
+        // Create multiple students
+        println("Creating 3 student objects ...")
+        val student1 = Student("John", "Doe", "john.doe@gmail.com")
+        val student2 = Student("Mary", "Public", "mary.public@gmail.com")
+        val student3 = Student("Bonita", "Applebum", "bonita.applebum@gmail.com")
+
+        // Save the student objects
+        println("Saving the students ...")
+        studentDao.save(student1)
+        studentDao.save(student2)
+        studentDao.save(student3)
+    }
+
+    private fun createStudent(studentDao: StudentDao) {
+        // Create the student object
+        println("Creating new student object ...")
+        val student = Student("Ali", "Mansour", "dev.ali.mansour@gmail.com")
+
+        // Save the student object
+        println("Saving the student ...")
+        studentDao.save(student)
+
+        // Display id of the saved student
+        println("Saved student. Generated id: " + student.id)
     }
 }
 
