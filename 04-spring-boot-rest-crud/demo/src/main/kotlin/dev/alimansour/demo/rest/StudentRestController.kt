fca8a4eac4ff05f9ceac95c0c51d338af4ebdb8c
Ali Mansour <dev.ali.mansour@gmail.com>
2026-07-16 20:02:48 +0300

    feat: Add generic exception handler to catch all exceptions
    - Implement a catch-all `@ExceptionHandler` method in `StudentRestController` to handle generic `Exception`s.
    - Create and return a `StudentErrorResponse` with an HTTP `BAD_REQUEST` status, the exception message, and the current timestamp.

--- 04-spring-boot-rest-crud/demo/src/main/kotlin/dev/alimansour/demo/rest/StudentRestController.kt
+++ 04-spring-boot-rest-crud/demo/src/main/kotlin/dev/alimansour/demo/rest/StudentRestController.kt
@@ -65,4 +65,22 @@ class StudentRestController {
 
         return ResponseEntity(error, HttpStatus.NOT_FOUND)
     }
+
+    // add another exception handler ... to catch any exception (catch all)
+
+    @ExceptionHandler
+    fun handleException(exc: Exception): ResponseEntity<StudentErrorResponse> {
+
+        // create StudentErrorResponse
+
+        val error = StudentErrorResponse(
+            status = HttpStatus.BAD_REQUEST.value(),
+            message = exc.message.orEmpty(),
+            timeStamp = System.currentTimeMillis()
+        )
+
+        // return ResponseEntity
+
+        return ResponseEntity(error, HttpStatus.BAD_REQUEST)
+    }
 }
