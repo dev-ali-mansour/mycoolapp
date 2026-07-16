a0b465d5bed1fa1eed0492a99db8b163f18255a3
Ali Mansour <dev.ali.mansour@gmail.com>
2026-07-16 18:04:38 +0300

    feat: Add endpoint to retrieve a single student by ID
    - Implement a `getStudent()` method mapped to `@GetMapping("students/{studentId}")` to handle fetching individual students.
    - Use the `@PathVariable` annotation to extract the `studentId` from the URL and return the `Student` located at that index in the list.

--- 04-spring-boot-rest-crud/demo/src/main/kotlin/dev/alimansour/demo/rest/StudentRestController.kt
+++ 04-spring-boot-rest-crud/demo/src/main/kotlin/dev/alimansour/demo/rest/StudentRestController.kt
@@ -3,6 +3,7 @@ package dev.alimansour.demo.rest
 import dev.alimansour.demo.entity.Student
 import jakarta.annotation.PostConstruct
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -27,5 +28,14 @@ class StudentRestController {
     @GetMapping("/students")
     fun getStudents(): List<Student> {
         return students
+    }
+
+    // define endpoint for "/students/{studentId}" - return Student at index
+
+    @GetMapping("students/{studentId}")
+    fun getStudent(@PathVariable studentId: Int): Student {
+
+        // just index into the list ... keep it simple for now
+        return students[studentId]
     }
 }
