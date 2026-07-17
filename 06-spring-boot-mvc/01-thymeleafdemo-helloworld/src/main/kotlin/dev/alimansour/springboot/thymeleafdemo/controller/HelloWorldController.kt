af23c82c933bf5c20bf5c8264eb3307b893f0180
Ali Mansour <dev.ali.mansour@gmail.com>
2026-07-17 19:06:59 +0300

    refactor: Update form processing to use POST method
    Changes the form submission method from GET to POST in the
    helloworld-form.html template. The corresponding controller
    endpoint is updated to use the @PostMapping annotation instead of
    @RequestMapping to strictly handle POST requests.

--- 06-spring-boot-mvc/01-thymeleafdemo-helloworld/src/main/kotlin/dev/alimansour/springboot/thymeleafdemo/controller/HelloWorldController.kt
+++ 06-spring-boot-mvc/01-thymeleafdemo-helloworld/src/main/kotlin/dev/alimansour/springboot/thymeleafdemo/controller/HelloWorldController.kt
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 
@@ -31,7 +32,7 @@ class HelloWorldController {
         return "helloworld"
     }
 
-    @RequestMapping("processFormVersionThree")
+    @PostMapping("processFormVersionThree")
     fun processFromVersionThree(
         @RequestParam("studentName") name: String,
         model: Model
