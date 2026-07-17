17e2687ffd7e81bd604d6d96eb7b706fed4c7914
Ali Mansour <dev.ali.mansour@gmail.com>
2026-07-17 11:03:26 +0300

    refactor: Remove custom repository REST resource mapping

--- 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-start/src/main/kotlin/dev/alimansour/springboot/cruddemo/dao/EmployeeRepository.kt
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-start/src/main/kotlin/dev/alimansour/springboot/cruddemo/dao/EmployeeRepository.kt
@@ -4,5 +4,4 @@ import dev.alimansour.springboot.cruddemo.entity.Employee
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
-@RepositoryRestResource(collectionResourceRel = "members", path = "members")
 interface EmployeeRepository : JpaRepository<Employee, Long>
