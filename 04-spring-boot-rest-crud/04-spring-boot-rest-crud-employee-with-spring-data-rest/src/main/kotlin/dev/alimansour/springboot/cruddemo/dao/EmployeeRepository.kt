768cbc728944087f3ad8424753576afd43e1abc9
Ali Mansour <dev.ali.mansour@gmail.com>
2026-07-17 09:39:33 +0300

    feat: Customize Spring Data REST path for EmployeeRepository
    This updates the EmployeeRepository to use 'members' as the resource path instead of the default 'employees' by applying the @RepositoryRestResource annotation.

--- 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/kotlin/dev/alimansour/springboot/cruddemo/dao/EmployeeRepository.kt
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/kotlin/dev/alimansour/springboot/cruddemo/dao/EmployeeRepository.kt
@@ -2,5 +2,7 @@ package dev.alimansour.springboot.cruddemo.dao
 
 import dev.alimansour.springboot.cruddemo.entity.Employee
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
+@RepositoryRestResource(collectionResourceRel = "members", path = "members")
 interface EmployeeRepository : JpaRepository<Employee, Long>
