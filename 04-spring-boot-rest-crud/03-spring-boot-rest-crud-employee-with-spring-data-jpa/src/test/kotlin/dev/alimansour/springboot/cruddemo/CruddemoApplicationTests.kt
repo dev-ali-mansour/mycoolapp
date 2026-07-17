1f95aa778671f78f671ae35a6583c7f96af65c21
Ali Mansour <dev.ali.mansour@gmail.com>
2026-07-17 08:18:59 +0300

    chore: Initialize Spring Boot REST CRUD project
    Set up the base Spring Boot application structure including the Maven
    wrapper, POM, application properties, and the main application class.

---
+++ 04-spring-boot-rest-crud/03-spring-boot-rest-crud-employee-with-spring-data-jpa/src/test/kotlin/dev/alimansour/springboot/cruddemo/CruddemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.alimansour.springboot.cruddemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
