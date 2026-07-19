05991205734861e1855a5a4ef0c4f241eee978a9
Ali Mansour <dev.ali.mansour@gmail.com>
2026-07-19 12:21:49 +0300

    chore: Initialize Spring Boot Validation demo project

---
+++ 06-spring-boot-mvc/validationdemo/src/main/kotlin/dev/alimansour/springdemo/mvc/ValidationdemoApplication.kt
@@ -0,0 +1,11 @@
+package dev.alimansour.springdemo.mvc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ValidationdemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<ValidationdemoApplication>(*args)
+}
---
+++ 06-spring-boot-mvc/validationdemo/src/test/kotlin/dev/alimansour/springdemo/mvc/ValidationdemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.alimansour.springdemo.mvc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ValidationdemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
