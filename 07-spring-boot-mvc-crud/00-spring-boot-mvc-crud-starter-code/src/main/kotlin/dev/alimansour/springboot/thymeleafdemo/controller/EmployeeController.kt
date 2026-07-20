1c1273faee414c9113838198e0f62a5dd99c896c
Ali Mansour <dev.ali.mansour@gmail.com>
2026-07-20 13:01:18 +0300

    feat: Add employee form and save functionality
    Provide a web interface for users to add new employees to the directory. This includes the necessary controller endpoints for displaying the form and processing the submission.

--- 07-spring-boot-mvc-crud/00-spring-boot-mvc-crud-starter-code/src/main/kotlin/dev/alimansour/springboot/thymeleafdemo/controller/EmployeeController.kt
+++ 07-spring-boot-mvc-crud/00-spring-boot-mvc-crud-starter-code/src/main/kotlin/dev/alimansour/springboot/thymeleafdemo/controller/EmployeeController.kt
@@ -1,21 +1,42 @@
 package dev.alimansour.springboot.thymeleafdemo.controller
 
+import dev.alimansour.springboot.thymeleafdemo.entity.Employee
 import dev.alimansour.springboot.thymeleafdemo.service.EmployeeService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 @RequestMapping("/employees")
 class EmployeeController(employeesService: EmployeeService, private val employeeService: EmployeeService) {
-        @GetMapping("/list")
+    @GetMapping("/list")
     fun listEmployees(model: Model): String {
         val employees = employeeService.findAll()
 
         model.addAttribute("employees", employees)
 
-        return "list-employees"
+        return "employees/list-employees"
+    }
+
+    @GetMapping("showFormForAdd")
+    fun showFormForAdd(model: Model): String {
+        val employee = Employee()
+
+        model.addAttribute("employee", employee)
+
+        return "employees/employee-form"
+    }
+
+    @PostMapping("/save")
+    fun saveEmployee(@ModelAttribute("employee") employee: Employee): String {
+
+        employeeService.save(employee)
+
+        return "redirect:/employees/list"
     }
 
 }
