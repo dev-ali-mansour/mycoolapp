fa9a18f9504a5bab1fc6e3964de842b7060096e0
Ali Mansour <dev.ali.mansour@gmail.com>
2026-07-21 09:42:51 +0300

    chore: Initialize Spring Boot Security demo project
    This adds the initial project structure and dependencies for the demosecurity Spring Boot application, including Spring Security, Thymeleaf, and Spring Web.

---
+++ 08-spring-boot-mvc-security/demosecurity/src/main/kotlin/dev/alimansour/springboot/demosecurity/DemosecurityApplication.kt
@@ -0,0 +1,11 @@
+package dev.alimansour.springboot.demosecurity
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemosecurityApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemosecurityApplication>(*args)
+}
