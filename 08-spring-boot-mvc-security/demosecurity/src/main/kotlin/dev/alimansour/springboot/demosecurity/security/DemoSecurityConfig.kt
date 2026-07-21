07dc32d239bf6a6077a3a808517228b37994f3f7
Ali Mansour <dev.ali.mansour@gmail.com>
2026-07-21 17:52:14 +0300

    feat: Use JDBC for authentication
    Update DemoSecurityConfig to replace the InMemoryUserDetailsManager with
    a JdbcUserDetailsManager. This configures Spring Security to authenticate
    users against the database using the provided DataSource. The previous
    in-memory user configuration has been commented out.

--- 08-spring-boot-mvc-security/demosecurity/src/main/kotlin/dev/alimansour/springboot/demosecurity/security/DemoSecurityConfig.kt
+++ 08-spring-boot-mvc-security/demosecurity/src/main/kotlin/dev/alimansour/springboot/demosecurity/security/DemoSecurityConfig.kt
@@ -3,32 +3,16 @@ package dev.alimansour.springboot.demosecurity.security
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UserDetails
-import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.provisioning.JdbcUserDetailsManager
+import org.springframework.security.provisioning.UserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
+import javax.sql.DataSource
 
 @Configuration
 class DemoSecurityConfig {
     @Bean
-    fun userDetailsManager(): InMemoryUserDetailsManager {
-        val john: UserDetails = User.builder()
-            .username("john")
-            .password("{noop}test123")
-            .roles("EMPLOYEE")
-            .build()
-        val mary: UserDetails = User.builder()
-            .username("mary")
-            .password("{noop}test123")
-            .roles("EMPLOYEE", "MANAGER")
-            .build()
-        val susan: UserDetails = User.builder()
-            .username("susan")
-            .password("{noop}test123")
-            .roles("EMPLOYEE", "MANAGER", "ADMIN")
-            .build()
-
-        return InMemoryUserDetailsManager(john, mary, susan)
+    fun userDetailsManager(dataSource: DataSource): UserDetailsManager {
+        return JdbcUserDetailsManager(dataSource)
     }
 
     @Bean
@@ -54,4 +38,25 @@ class DemoSecurityConfig {
 
         return http.build()
     }
+
+    /*@Bean
+    fun userDetailsManager(): InMemoryUserDetailsManager {
+        val john: UserDetails = User.builder()
+            .username("john")
+            .password("{noop}test123")
+            .roles("EMPLOYEE")
+            .build()
+        val mary: UserDetails = User.builder()
+            .username("mary")
+            .password("{noop}test123")
+            .roles("EMPLOYEE", "MANAGER")
+            .build()
+        val susan: UserDetails = User.builder()
+            .username("susan")
+            .password("{noop}test123")
+            .roles("EMPLOYEE", "MANAGER", "ADMIN")
+            .build()
+
+        return InMemoryUserDetailsManager(john, mary, susan)
+    }*/
 }
